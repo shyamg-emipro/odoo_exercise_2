82e9d76e0fd54a9f2f1a34bb2c3f7ecab57561d6
shyamg <shyamg@emiprotechnologies.com>
2022-02-15 19:19:27 +0530

    [Mod] Requirement 15

--- sale_order_extended/models/crm_lead.py
+++ sale_order_extended/models/crm_lead.py
@@ -6,8 +6,9 @@ class CrmLeadExtended(models.Model):
     _inherit = "crm.lead"
 
     def action_new_quotation(self):
+        tag_ids = self.env.ref("sale_order_extended.crm_tags_sale_order_extended").id
         action = super(CrmLeadExtended, self).action_new_quotation()
-        # tag_ids = [(6, 0, action['context']['default_tag_ids'][0][2].append(self.env.ref("sale_order_extended.crm_tags_sale_order_extended").id))]
+        # tag_ids = [(6, 0, [list(action['context']['default_tag_ids'][0])[2][0], self.env.ref("sale_order_extended.crm_tags_sale_order_extended").id])]
         action['context']['default_lead_id'] = self.id
-        # action['context']['default_tag_ids'] = tag_ids
+        action['context']['default_tag_ids'] += [(4, tag_ids, 0)]
         return action
