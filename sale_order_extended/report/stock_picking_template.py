aa91c18bd5d75d4687319501c249279c641e20bb
shyamg <shyamg@emiprotechnologies.com>
2022-03-15 12:12:15 +0530

    [MOD] Req 24(report.sale_order_extended.stock_picking_custom_template) & 25(report.sale_order_extended.stock_picking_custom_template_2) added funtion get_grouped_do() removed existing funtion from Req 24

--- sale_order_extended/report/stock_picking_template.py
+++ sale_order_extended/report/stock_picking_template.py
@@ -4,33 +4,36 @@ from odoo import models, fields
 class StockPickingTemplate(models.AbstractModel):
     _name = "report.sale_order_extended.stock_picking_custom_template"
 
-    def get_product_delivery_orders(self, obj, location_id=False):
-        product_ids = obj.move_line_ids_without_package.product_id.ids
-        products = self.env['product.product'].browse(product_ids)
-        product_delivery_orders = {}
-        for product in products:
-            do_qty = {}
-            deliver_orders = obj.filtered(lambda d: product.id in d.move_line_ids_without_package.product_id.ids)
-            for do in deliver_orders:
-                if location_id:
-                    moves = do.move_line_ids_without_package.filtered(lambda move: move.product_id.id == product.id and move.location_id.id == location_id.id)
-                else:
-                    moves = do.move_line_ids_without_package.filtered(lambda move: move.product_id.id == product.id)
-                total_qty = sum([move.product_uom_qty for move in moves])
-                do_qty[do] = total_qty
-            product_delivery_orders[(product, product.product_template_attribute_value_ids,)] = do_qty
-        return product_delivery_orders
+    def get_grouped_do(self, selected_delivery_orders, group_by_location=True):
+        moves = selected_delivery_orders.move_line_ids_without_package
+        grouped_products = {}
+        for move in moves:
+            grouped_do = {}
+            location = ""
+            if group_by_location:
+                location = move.location_id.complete_name
+            found_grouped_products = grouped_products.get(location, {})
+            found_grouped_do = found_grouped_products.get((
+                move.product_id, move.product_id.product_template_attribute_value_ids), {})
+            product_qty = found_grouped_do.get(move.picking_id, 0)
+            found_grouped_do.update({move.picking_id: product_qty + move.product_uom_qty})
+            grouped_do[(move.product_id, move.product_id.product_template_attribute_value_ids)] = found_grouped_do
+            found_grouped_products.update(grouped_do)
+            grouped_products[location] = found_grouped_products
+        if group_by_location:
+            return grouped_products
+        return grouped_products[location]
 
     def _get_report_values(self, docids, data=None):
         # get the report action back as we will need its data
-        report = self.env['ir.actions.report']._get_report_from_name('sale_order_extended.stock_picking_custom_template')
+        report = self.env[
+            'ir.actions.report']._get_report_from_name('sale_order_extended.stock_picking_custom_template')
         # get the records selected for this rendering of the report
         obj = self.env['stock.picking'].browse(docids)
-        product_do_qty = self.get_product_delivery_orders(obj)
         # return a custom rendering context
         return {
             'docs': obj,
             'doc_model': 'stock.picking',
             'doc_ids': docids,
-            'product_do_qty': product_do_qty,
+            'self': self
         }
--- sale_order_extended/report/stock_picking_views_ext_2.py
+++ sale_order_extended/report/stock_picking_views_ext_2.py
@@ -4,8 +4,11 @@ from odoo import models, fields
 class StockPickingTemplate(models.AbstractModel):
     _name = "report.sale_order_extended.stock_picking_custom_template_2"
 
-    def get_products_do_by_location(self):
-        pass
+    def get_grouped_do(self, selected_delivery_orders, group_by_location=True):
+        report_value_rec = self.env['report.sale_order_extended.stock_picking_custom_template']
+        return report_value_rec.get_grouped_do(selected_delivery_orders, group_by_location)
+
+
 
     def _get_report_values(self, docids, data=None):
         # get the report action back as we will need its data
